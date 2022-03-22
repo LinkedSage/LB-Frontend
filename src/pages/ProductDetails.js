a031425d7cea79d8d91520ba1dc63f3a9135304a
muqtadir-61 <muqtadir.ahmed@linkedsage.com>
2022-03-22 11:00:28 +0600

    Perviews code commiting

---
+++ src/pages/ProductDetails.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../Components/CSS/ProductDetails.css";
+
+function ProductDetails() {
+  return <div>ProductDetails</div>;
+}
+
+export default ProductDetails;
--- src/section/Section.js
+++ src/section/Section.js
@@ -4,17 +4,16 @@ import Navbar from "../Components/Navbar";
 import ProtectedRoute from "../Components/ProtectedRoute";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
-
-
+import ProductDetails from "../pages/ProductDetails";
 
 export default function Section() {
   return (
     <>
       <Route path="/login" component={Login} />
-     
-      <Route exact path = "/" component={Home} />
 
-      
+      <Route exact path="/" component={Home} />
+      <Route exact path="/product_derails" component={ProductDetails} />
+
       {/* <ProtectedRoute exact path = "/top-user-by-country" component={topUserByCountry} /> */}
     </>
   );
